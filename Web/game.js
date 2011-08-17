53548dab8e06746fca956500d03fc27d965c4c7d
Matt Carter <m@ttcarter.com>
2011-08-17 11:59:48 +1000

    Fleshed out Web interface

--- Web/game.js
+++ Web/game.js
@@ -3,37 +3,68 @@ $(function() {
 	var animals = [
 		{
 			name: 'Tiger',
-			keys: [65, 66, 67]
+			img: 'images/tiger.gif',
+			keys: ["Q".charCodeAt(0), "W".charCodeAt(0), "P".charCodeAt(0), "O".charCodeAt(0)]
 		},
 		{
-			name: 'Panda',
-			keys: [99, 100, 101]
+			name: 'Bear',
+			img: 'images/bear.jpg',
+			keys: ["A".charCodeAt(0), "S".charCodeAt(0), "L".charCodeAt(0), "K".charCodeAt(0)]
 		},
 		{
-			name: 'Turtle',
-			keys: [102, 103, 104]
+			name: 'Monkey',
+			img: 'images/monkey.jpg',
+			keys: ["Z".charCodeAt(0), "X".charCodeAt(0)]
 		}
 	];
+	var animationspeed = 1000;
 	var activeanimal = -1;
+	var companimal = -1;
+	var ignoreselect = 0;
 
 	$(document).bind('keydown', function(e) {
 		var code = (e.keyCode ? e.keyCode : e.which);
 		if (keys[code] == 1) // We already know about this key being down
 			return;
 		keys[code] = 1;
-		console.log('DN key ' + code);
+		if (ignoreselect)
+			return;
+		console.log('Key hold for ' + code + ' - ' + String.fromCharCode(code));
 		for (var a = 0; a < animals.length; a++) {
 			var passed = 0;
 			for (var k = 0; k < animals[a]['keys'].length; k++) {
-				console.log('Check for animal ' + a + ' @ key ' + animals[a]['keys'][k] + ' == ' + keys[animals[a]['keys'][k]]);
 				if (keys[animals[a]['keys'][k]] == 1) {
 					passed++;
 				} else
 					break;
 			}
 			if (passed == animals[a]['keys'].length) { // Activate this animal
-				console.log('Trigger for animal ' + animals[a]['name']);
+				console.log('Trigger for ' + animals[a]['name']);
 				activeanimal = a;
+				ignoreselect = 1;
+				$('#play_0').hide();
+				$('#play_0').html('<img src="' + animals[a]['img'] + '"/>');
+				$('#play_0').fadeIn(animationspeed, function() {
+					ignoreselect = 0;	
+
+					// Computer plays
+					companimal = Math.floor(Math.random() * 3);
+					$('#play_1').hide();
+					$('#play_1').html('<img src="' + animals[companimal]['img'] + '"/>');
+					$('#play_1').fadeIn(animationspeed);
+
+					if (activeanimal == companimal) { // Draw test
+						$('#result').html('Draws with');
+					} else if ( // Win test (Unrolled logic for the sake of simplicity)
+						(activeanimal == 0 && companimal == 1) ||
+						(activeanimal == 1 && companimal == 2) ||
+						(activeanimal == 2 && companimal == 0)
+					) {
+						$('#result').html('Beats');
+					} else {
+						$('#result').html('Loses to');
+					}
+				});
 				break;
 			}
 		}
@@ -42,6 +73,13 @@ $(function() {
 		var code = (e.keyCode ? e.keyCode : e.which);
 		console.log('UP key ' + code);
 		keys[code] = 0;
+		if (ignoreselect)
+			return;
 		activeanimal = -1;
 	});
+
+	// Setup img pre-loader
+	for (var a = 0; a < animals.length; a++) {
+		$('#preload').append('<img src="' + animals[a]['img'] + '"/>');
+	}
 });
